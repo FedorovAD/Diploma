ac8767ebeb0ac67e176d8929998fa106303edbd0
Mediocrity <mediocrity@Bubuntu.myguest.virtualbox.org>
2024-03-15 23:51:01 +0300

    cheta sdelal

--- db/migrations/V001__user.sql
+++ db/migrations/V001__user.sql
@@ -1,4 +1,19 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS places(
 	id serial PRIMARY KEY,
-	name TEXT NOT NULL
+	name TEXT NOT NULL,
+	place_type TEXT NOT NULL,
+	address	TEXT NOT NULL,
+	latitude numeric,
+	longitude numeric,
+	price integer,
+	score numeric,
+	link TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS work_time (
+	place_id integer NOT NULL REFERENCES(places.id),
+	id serial PRIMARY KEY,
+	day_of_week TEXT NOT NULL,
+	open_time timestamptz,
+	close_time timestamptz
 );
---
+++ src/app/routes/v1/data-get.ts
@@ -0,0 +1,33 @@
+import got from 'got';
+interface Fields{
+    CommonName: string,
+    Category: string,
+    ObjectAddress: {Address: string}[],
+    geoData: {coordinates: number[][]},
+    WebSite: string,
+    WorkingHours: {
+        DayWeek: string,
+        WorkHours: string
+    }[]
+}
+
+interface OverFields{
+    Cells: Fields
+}
+
+(async function MosRuGet() {
+    const data: OverFields[] = await got.get('https://apidata.mos.ru/v1/datasets/529/rows', {
+    searchParams:{
+        api_key: 'a131d934-ffcc-4e3a-a3db-b28277d80188'
+    }
+    }).json();
+    const result: Fields[] = data.map((field) => ({
+        CommonName: field.Cells.CommonName,
+        Category: field.Cells.Category,
+        ObjectAddress: field.Cells.ObjectAddress.map((item) => ({Address: item.Address})),
+        geoData: field.Cells.geoData,
+        WebSite: field.Cells.WebSite,
+        WorkingHours: field.Cells.WorkingHours
+    }))
+    console.log(JSON.stringify(result, null, 2));
+})()
