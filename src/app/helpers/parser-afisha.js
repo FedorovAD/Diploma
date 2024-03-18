795f2c1c1a09ec7e3b47dd521621f906d5ab77ac
FedorovAD <alex_fed02@mail.ru>
2024-03-18 21:46:14 +0300

    created parser for afisha.ru, that gives us inform ation about museum name. location and link.

---
+++ src/app/helpers/parser-afisha.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const cheerio = require('cheerio')
+
+const parse = async () => {
+    const getHTML = async (url) => {
+        const {data} = await axios.get(url);
+        return cheerio.load(data);
+    };
+    const website = await getHTML('https://www.afisha.ru/msk/museum/');
+    const pageNumber = website('.yNWhI').eq(-1).text();
+    for (let i = 0; i < pageNumber; i++){
+        const selector = await getHTML(`https://www.afisha.ru/msk/museum/page${i + 1}/`)
+        selector('div._yjkz').each((i, element) => {
+            const title = selector(element).find('a.CjnHd.y8A5E.MnbCM').text()
+            const titleLink = selector(element).find('a.CjnHd.y8A5E.MnbCM').attr('href')
+            const titleAddress = selector(element).find('.hmVRD.DiLyV').text()
+
+
+            //const title = selector(element).find('div.PqAdz').find('div._yjkz').find('a.CjnHd.y8A5E.MnbCM').text();
+            //const titleLink = selector(element).find('div.PqAdz').find('div._yjkz').find('a.CjnHd.y8A5E.MnbCM').find('#href').text();
+            console.log(title, ` `, titleAddress ,` `, `https://www.afisha.ru/msk/museum/${titleLink}`)
+        })
+    }
+
+}
+
+parse()
