d1bfc83eab62d5be8e7b6347ac9623b87d58f4ba
FedorovAD <alex_fed02@mail.ru>
2024-03-19 03:10:18 +0300

    coco jambo

--- src/app/helpers/parser-afisha.js
+++ src/app/helpers/parser-afisha.js
@@ -14,11 +14,12 @@ const parse = async () => {
             const title = selector(element).find('a.CjnHd.y8A5E.MnbCM').text()
             const titleLink = selector(element).find('a.CjnHd.y8A5E.MnbCM').attr('href')
             const titleAddress = selector(element).find('.hmVRD.DiLyV').text()
+            const titleScore = selector(element).find('.IrSqF.zPI3b._49ey.k96pX').text()
 
 
             //const title = selector(element).find('div.PqAdz').find('div._yjkz').find('a.CjnHd.y8A5E.MnbCM').text();
             //const titleLink = selector(element).find('div.PqAdz').find('div._yjkz').find('a.CjnHd.y8A5E.MnbCM').find('#href').text();
-            console.log(title, ` `, titleAddress ,` `, `https://www.afisha.ru/msk/museum/${titleLink}`)
+            console.log('Название: ', title, ` Адрес: `, titleAddress ,` Ссылка: `, `https://www.afisha.ru/msk/museum/${titleLink}`, ' Оценки пользователей: ', titleScore)
         })
     }
 
